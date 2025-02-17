e297820a0add692685bc40e9fab1c2e96013c19d
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2025-02-17 15:08:24 -0600

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if (empty($_SESSION['authenticated'])) {
+header('Location: index.php');
+ exit();
+ }
+?>
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The St. Anthony Email Signature Generator</title>
+  
+  <!-- Favicon -->
+  <link rel="icon" href="images/SATLC-favicon.png" type="image/png">
+  
+  <!-- Open Graph -->
+  <meta property="og:image" content="images/SATLC-email-sig-generator-opengraph.png">
+  
+  <!-- Bulma CSS for Layout & Form Styling -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+  <!-- Font Awesome (only used in input fields and mobile warning icon) -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  
+  <!-- CSS for authorized.php -->
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+  <!-- Header Section with St Anthony Hotel Logo -->
+  <header class="header">
+    <img src="images/SATLC-Logo.png" alt="St Anthony Hotel Logo" width="125" style="display:block; margin:0 auto 10px;">
+    <br>
+    <h1>Employee Signature Generator</h1>
+  </header>
